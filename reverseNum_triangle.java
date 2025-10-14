8e1b468cc91593385f69a3c52cc10634660a88a0
Gautam99-coder <gautamtharu1000@gmail.com>
2025-10-14 23:54:26 +0530

    pattern 2

--- reverseNum_triangle.java
+++ reverseNum_triangle.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class reverseNum_triangle {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=n; i++){
+            for(int j=i; j>=1; j--){
+                System.out.print(j +" ");
+            }
+            System.out.println();
+        }
+    }
 }
